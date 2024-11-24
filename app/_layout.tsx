dbab6537ea292426aa48bb96407c5e73d03e0aa5
Tejasvi raj <tejasviraj8686@gmail.com>
2024-11-24 15:51:32 +0530

    implement react dev mode

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -14,7 +17,8 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    SegoeUI: require("../assets/fonts/Segoe-UI.ttf"),
   });
 
   useEffect(() => {
@@ -28,12 +32,16 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index"  />
-        <Stack.Screen name="/(routes)/onboarding/index"  />
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="index" />
+        <Stack.Screen name="(routes)/onboarding/index" />
+        <Stack.Screen name="(routes)/home/index" />
       </Stack>
-      <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,25 @@
-import { View, Text } from "react-native";
 import React, { useEffect, useState } from "react";
 import { Redirect } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function index() {
-  // const [isonboarding, setIsOnboarding] = useState(false);
-  // useEffect(() => {
-  //   setIsOnboarding(true);
-  // }, []);
-  return <Redirect href="/(routes)/onboarding" />;
+  const [isOnboarding, setIsOnboarding] = useState(true);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkOnboarding = async () => {
+      const isOnboarding = await AsyncStorage.getItem("onboarding");
+      if (isOnboarding) {
+        setIsOnboarding(false);
+      }
+      setLoading(false);
+    };
+    checkOnboarding();
+  }, []);
+
+  if (loading) return null;
+
+  return (
+    <Redirect href={isOnboarding ? "/(routes)/onboarding" : "/(routes)/home"} />
+  );
 }
